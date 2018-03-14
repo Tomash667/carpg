7de5863fcf704d8ec5636a53ea3e5947a416f0e5
Tomashu <tomash4my@gmail.com>
2018-03-14 22:55:04 +0100

    fixed #238 leaving level with units dying, fixed crash when pvp target quits (lose sound when quiting)

--- project/source/game/Game.h
+++ project/source/game/Game.h
@@ -753,8 +753,6 @@ public:
 
 	//--------------------------------------
 	// ARENA
-	// etapy: 0-brak, 1-walka wchodzenie, 2-walka oczekiwanie, 3-walka, 4-walka oczekiwanie po, 5-walka koniec
-	// 10-pvp wchodzenie, 11-pvp oczekiwanie, 12-pvp 13-pvp oczekiwanie po, 14-pvp koniec
 	enum TrybArena
 	{
 		Arena_Brak,
@@ -788,6 +786,7 @@ public:
 	PlayerController* pvp_player;
 	vector<Unit*> arena_viewers;
 
+	void UpdateArena(float dt);
 	void CleanArena();
 
 	//--------------------------------------
@@ -809,7 +808,7 @@ public:
 	bool IfUnitJoinTournament(Unit& u);
 	void GenerateTournamentUnits();
 	void UpdateTournament(float dt);
-	void VerifyTournamentUnit(Unit& u);
+	void VerifyTournamentUnit(Unit* unit);
 	void StartTournamentRound();
 	void TournamentTalk(cstring text);
 	void TournamentTrain(Unit& u);
@@ -1188,7 +1187,7 @@ public:
 	};
 	Unit* SpawnUnitInsideInn(UnitData& unit, int level = -1, InsideBuilding* inn = nullptr, int flags = 0);
 	Unit* CreateUnitWithAI(LevelContext& ctx, UnitData& unit, int level = -1, Human* human_data = nullptr, const Vec3* pos = nullptr, const float* rot = nullptr, AIController** ai = nullptr);
-	void ChangeLevel(int gdzie);
+	void ChangeLevel(int where);
 	void AddPlayerTeam(const Vec3& pos, float rot, bool reenter, bool hide_weapon);
 	void OpenDoorsByTeam(const Int2& pt);
 	void ExitToMap();
